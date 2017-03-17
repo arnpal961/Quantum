1922e48e75ea9e93a86b7a15c78dcc66604cd37a
ARNAB PAL <arnab.ee.ju@gmail.com>
2017-03-17 22:45:53 +0530

    Add files via upload

---
+++ quantum.py
@@ -0,0 +1,34 @@
+'''
+Quantum
+A wrapper for kiteconnect
+'''
+
+import webbrowser as wb
+import pandas as pd
+from kiteconnect import KiteConnect,WebSocket
+api_key,api_secret,user_id = open('credential','r+').read().strip('\n').split(',')
+kite = KiteConnect(api_key=api_key)
+login_url = kite.login_url()
+wb.open(login_url)
+
+
+
+
+#Get the request token after login from webbrowser and put the token here
+request_token = ''
+auth_data = kite.request_access_token(request_token,secret=api_secret)
+access_token = auth_data["access_token"]
+public_token = auth_data["public_token"]
+kite.set_access_token(access_token)
+with open('auth_data.txt','r+') as f:
+    f.write(access_token+','+public_token)    
+
+
+#Retrieve instruments token
+instrument = kite.instruments()
+instrument = pd.DataFrame(instrument)
+instrument = instrument[instrument.exchange!= 'BSE']
+
+
+#Saving to local file
+instrument.to_csv('instrument.csv')
